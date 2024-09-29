2b693aba288ea361401f85c864b380bd2775a1b2
Dante Barboza <90792603+DanteCoder@users.noreply.github.com>
2024-09-29 11:46:25 -0600

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.strict],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.json'],
+    },
+  },
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+});
